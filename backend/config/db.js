bc0bc0faf657dd5070ffbf17b7f88ecc0427a1ec
sameerpatil05 <sameerpatil5106@gmail.com>
2026-05-01 23:09:58 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser:    true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`❌ MongoDB Error: ${err.message}`);
-    process.exit(1);
+
+    // ❗ DO NOT EXIT (important for deployment)
+    // process.exit(1); ❌ REMOVE THIS
   }
 };
 
